d167144cd2d71af79b45699e4ceae4a1254e3623
Art <a.muhrin@yandex.ru>
2021-08-02 16:15:53 +0300

    callback

--- src/main/java/com/example/callbackbot/service/KeyboardService.java
+++ src/main/java/com/example/callbackbot/service/KeyboardService.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-
 @Service
 public class KeyboardService {
 

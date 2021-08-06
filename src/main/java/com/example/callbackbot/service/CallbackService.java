36ea1bc1ef9dfe1cb6b5dbf5a5ffec09280264dd
Art <a.muhrin@yandex.ru>
2021-08-06 15:04:46 +0300

    callback

--- src/main/java/com/example/callbackbot/service/CallbackService.java
+++ src/main/java/com/example/callbackbot/service/CallbackService.java
@@ -3,12 +3,15 @@ package com.example.callbackbot.service;
 import com.example.callbackbot.model.CallbackEvent;
 import com.example.callbackbot.util.CallbackEventProcessor;
 import com.example.callbackbot.util.CallbackEventProcessorFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class CallbackService {
 
+    private static final Logger logger = LoggerFactory.getLogger(CallbackService.class);
     private final CallbackEventProcessorFactory callbackEventProcessorFactory;
 
     public CallbackService(CallbackEventProcessorFactory callbackEventProcessorFactory) {
@@ -17,6 +20,11 @@ public class CallbackService {
 
     public String processCallbackEvent(CallbackEvent callbackEvent) {
         CallbackEventProcessor callbackEventProcessor = callbackEventProcessorFactory.getEventProcessor(callbackEvent.getType());
-        return callbackEventProcessor.process(callbackEvent);
+        if (callbackEventProcessor == null) {
+            logger.warn("failed to create message processor.");
+            return "Something went wrong.";
+        } else {
+            return callbackEventProcessor.process(callbackEvent);
+        }
     }
 }
